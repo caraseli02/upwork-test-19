d701cf226cd946b7ab5d1c2f310421a0d8d05244
vlad <caraseli02@gmail.com>
2023-05-19 22:33:03 +0200

    feat: add title

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,24 @@
 </script>
 
 <template>
-  <h1></h1>
-  <section>
-  
-  </section>
+  <div class="pt-[108px]">
+    <h1 class="text-[56px] text-[#1B264F] w-[730px] m-auto text-center">
+      We make creative media that <span class="text-[#506BCA]">adds value</span>
+    </h1>
+  </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@600&family=Poppins:wght@700&display=swap');
+
+/* set the Poppins font for h tags */
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-family: 'Poppins', sans-serif;
 }
 </style>
